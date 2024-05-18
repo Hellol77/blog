0cd4ace76a28739e480b18591c17e95f54e66fa2
DongHyeonWon <dhe7700@naver.com>
2024-05-18 15:46:11 +0900

    feat: 폰트 및 contentLayer 세팅

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const { withContentlayer } = require("next-contentlayer");
+
+/** @type {import('next').NextConfig} */
+const options = {
+  reactStrictMode: true,
+  swcMinify: false,
+  // 옵션은 자유롭게 넣어주세요.
+};
+
+module.exports = withContentlayer(options);
