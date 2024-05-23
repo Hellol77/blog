8314fd764b6604190a369fd93ec7c41627f00835
DongHyeonWon <dhe7700@naver.com>
2024-05-23 21:14:58 +0900

    feat: 테마 변경 아이콘 추가

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Button } from "@nextui-org/react";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { Moon, Sun } from "lucide-react";
 
 const navigation = [
   { label: "Home", href: "/" },
@@ -22,14 +22,17 @@ export default function Nav() {
       {navigation.map((nav) => (
         <li key={nav.label}>
           <Link
-            className="duration-2000 hover:text-textBasic text-subText font-light transition"
+            className="duration-2000 font-light text-subText transition hover:text-textBasic"
             href={nav.href}
           >
             {nav.label}
           </Link>
         </li>
       ))}
-      <Button onClick={toggleTheme}>change</Button>
+      <button className="h-12 w-12" onClick={toggleTheme}>
+        <Sun className="h-5 w-5 transition-all dark:hidden" />
+        <Moon className="hidden h-5 w-5 transition-all dark:block" />
+      </button>
     </ul>
   );
 }
